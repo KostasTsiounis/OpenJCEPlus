ddac9f1a605185c4e1b45d7798c229a1cde79f9f
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2025-01-30 10:30:21 -0500

    Remove local copy of CurveDB

--- src/main/java/com/ibm/crypto/plus/provider/ECParameters.java
+++ src/main/java/com/ibm/crypto/plus/provider/ECParameters.java
@@ -27,6 +27,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import sun.security.util.CurveDB;
 import sun.security.util.DerInputStream;
 import sun.security.util.DerOutputStream;
 import sun.security.util.DerValue;
