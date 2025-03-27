cdb3d9215af2832234df9b86ba66993125803f16
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2025-03-27 19:21:59 -0400

    Fix checkstyle errors 3

--- src/main/java/com/ibm/crypto/plus/provider/OpenJCEPlusProvider.java
+++ src/main/java/com/ibm/crypto/plus/provider/OpenJCEPlusProvider.java
@@ -8,8 +8,6 @@
 
 package com.ibm.crypto.plus.provider;
 
-import java.security.ProviderException;
-
 // Internal interface for OpenJCEPlus and OpenJCEPlus implementation classes.
 // Implemented as an abstract class rather than an interface so that 
 // methods can be package protected, as interfaces have only public methods.
