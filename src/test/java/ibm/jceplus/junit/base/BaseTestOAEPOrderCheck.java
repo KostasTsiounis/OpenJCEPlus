85cf1b030a28986f979626c63970fa995b6121ea
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2025-04-22 10:49:23 -0400

    Add copyrights

--- src/test/java/ibm/jceplus/junit/base/BaseTestOAEPOrderCheck.java
+++ src/test/java/ibm/jceplus/junit/base/BaseTestOAEPOrderCheck.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
 package ibm.jceplus.junit.base;
 
 import java.io.IOException;
