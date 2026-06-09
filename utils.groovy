67c9494923df8f45866f3ebcb36cb4cea42f9202
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2026-06-09 13:43:24 -0400

    Change openssl version 2

--- utils.groovy
+++ utils.groovy
@@ -285,7 +285,7 @@ def getMaven(software) {
 }
 
 def getOpenSSL(hardware, software) {
-    def version = "openssl-4.0.0"
+    def version = "openssl-3.6.3"
     def platform = "${hardware}_${software}"
     //sh "git clone -b ${version} https://github.com/openssl/openssl.git"
 
