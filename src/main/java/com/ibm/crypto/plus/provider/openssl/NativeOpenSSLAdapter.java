35fb5f66b8da6f787167decad0b8a0602eb22dc2
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2026-08-06 13:10:02 -0400

    Fix wrong signature

--- src/main/java/com/ibm/crypto/plus/provider/openssl/NativeOpenSSLAdapter.java
+++ src/main/java/com/ibm/crypto/plus/provider/openssl/NativeOpenSSLAdapter.java
@@ -1104,9 +1104,9 @@ public abstract class NativeOpenSSLAdapter implements NativeInterface {
     }
 
     @Override
-    public byte[] XECKEY_computeECDHSecret(long genCtx, long pubEcKeyId, long privEcKeyId, int secrectBufferSize)
+    public byte[] XECKEY_computeECDHSecret(long genCtx, long pubEcKeyId, long privEcKeyId)
             throws OpenSSLException {
-        return NativeOpenSSLImplementation.XECKEY_computeECDHSecret(osslContext.getId(), genCtx, pubEcKeyId, privEcKeyId, secrectBufferSize);
+        return NativeOpenSSLImplementation.XECKEY_computeECDHSecret(osslContext.getId(), genCtx, pubEcKeyId, privEcKeyId);
     }
 
     @Override
